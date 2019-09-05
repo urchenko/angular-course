c01b992c142d728b8c6d0d04b222eebabd0156db
urchenkomaksim <urchenkomaksim@gmail.com>
2019-09-05 07:13:59 +0300

    Aded warning and success alerts

---
+++ src/app/success-alert/success-alert.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+    // tslint:disable-next-line:component-selector
+    selector: '[app-success-alert]',
+    templateUrl: './success-alert.component.html',
+    styleUrls: ['./success-alert.component.scss'],
+})
+export class SuccessAlertComponent {}
