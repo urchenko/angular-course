7554b50927532b1cc5326b64c7f1f9d601469f02
Maksim <urchenkomaksim@gmail.com>
2019-10-15 05:57:27 +0300

    Prepared for directive deep dive

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,7 +3,9 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  numbers = [1, 2, 3, 4, 5];
+  onlyOdd = false;
 }
