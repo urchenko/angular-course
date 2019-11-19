1e47c61d0f45f0276daf2b33113d3ee399a3e998
Maksim <urchenkomaksim@gmail.com>
2019-11-19 06:56:05 +0200

    Provided template-driven aproach

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild, ElementRef } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,27 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+@ViewChild('form', {static: false}) signupForm: NgForm;
+public defaultSubscription: string = 'Advanced';
+public user = {
+  inputEmail: '',
+  subscriptions: '',
+  inputPassword: '',
+};
+public submited = false;
 
+  // onSubmit(form: NgForm) {
+  //   console.log(form);
+  // }
+
+  // onSubmit(form: ElementRef) {
+  //   console.log(form);
+  // }
+
+  onSubmit() {
+    this.submited = true;
+    this.user.inputEmail = this.signupForm.value.inputEmail;
+    this.user.subscriptions = this.signupForm.value.subscriptions;
+    this.user.inputPassword = this.signupForm.value.inputPassword;
+  }
 }
